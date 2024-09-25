87df7fd7918811cf549f5ab787befe72fa9e11cc
HartEnzo <enzoguihartmann@gmail.com>
2024-09-25 11:32:58 -0300

    feat:add token

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { signup, login } from "../controllers/user-controller";
+import { signup, login } from "../controllers/user-controller.js";
 
 const router = Router();
 
