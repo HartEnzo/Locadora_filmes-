12e5883efcf9b1b24bfba116f805b8aecb59f64a
HartEnzo <enzoguihartmann@gmail.com>
2024-09-25 08:55:31 -0300

    feat: fix bug

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -1,17 +1,21 @@
-const store = (req, res) => {
+import Movie from "../models/movie-model.js"
+
+export const store = async (req, res) => {
     try {
-        const content = await Movie.
+        const content = await Movie .create(req.body);
+        
         res.status(201).josn(content)
     }catch (error) {
         res.status(400).send(error)
     }
 };
 
-const index = async (req, res) => {
+export const index = async (req, res) => {
     try {
         const content = await Movie.find({
             renteBy: undefined
         }).exec();
+        
         res.json(content);
     } catch (error) {
         res.status(400).send(error);
